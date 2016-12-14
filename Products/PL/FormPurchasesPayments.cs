017ab655a9b99a6f5e216dbf83396eca15d270b9
eslam <eslam@ESLAMMAHFOUZ>
2016-12-14 21:31:00 +0200

    add Purchase - design

--- Products/PL/FormPurchasesPayments.cs
+++ Products/PL/FormPurchasesPayments.cs
@@ -16,5 +16,10 @@ namespace Products.PL
         {
             InitializeComponent();
         }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
